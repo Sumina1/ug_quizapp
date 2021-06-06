a335a7f652a4654c973e6f883e0d2c8c6b043db5
Sumina Khatiwada <suk0480@my.londonmet.ac.uk>
2021-06-06 15:27:32 +0545

    data fetched

--- lib/fetechapi.dart
+++ lib/fetechapi.dart
@@ -9,18 +9,18 @@ class FetchApi {
 
   Future<Welcome> fetechdata() async {
     final response =
-    await http.get(Uri.parse('https://staging.ugbazaar.com/api/V2'));
+    await http.get(Uri.parse('https://staging.ugbazaar.com/api/V2/quiz'));
+    print(response.statusCode);
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
       // then parse the JSON.
-      return Welcome.fromJson(jsonDecode(response.body));
+      var data = Welcome.fromJson(jsonDecode(response.body));
+      return data;
     } else {
       // If the server did not return a 200 OK response,
       // then throw an exception.
       throw Exception('Failed to load ');
     }
-
-
   }
 
 }
